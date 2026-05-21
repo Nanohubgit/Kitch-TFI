e678fb6c54d9be6a4798dffbc01cb83b76de2044
Nanohubgit <frisconano@gmail.com>
2026-05-21 09:55:18 -0300

    Primeras entidades creadas a nivel Domain

---
+++ Kitch.Domain/entities/Usuario.cs
@@ -0,0 +1,13 @@
+namespace Kitch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Contraseña { get; set; } = string.Empty;
+        public bool Activo { get; set; } = true;
+        public string Rol { get; set; } = "Basico"; // Puede ser Básico, Intermedio o Profesional
+    }
+}
