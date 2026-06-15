f683fd8d0f1db184ff4ba153740b7cff9cec402c
unknown <frisconano@gmail.com>
2026-06-15 19:09:00 -0300

    Implementación de Repository Genérico, interfaces y registro de DI

---
+++ Kitch.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using Kitch.Domain.Interfaces;
+using Kitch.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kitch.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        return services;
+    }
+}
